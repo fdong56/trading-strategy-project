5799043fa4d7e07fbb8caaf52986d5eb159d3bc7
fdong56 <202312821+fdong56@users.noreply.github.com>
2025-05-22 15:39:36 -0400

    update running args

--- run.py
+++ run.py
@@ -1,9 +1,115 @@
+import argparse
 import datetime
 import utility
 import matplotlib.pyplot as plt
-import DecisionTreeTrader as dtt
-import QLearningTrader as qlt
+from QLearningTrader import QLearningTrader
+from DecisionTreeTrader import DecisionTreeTrader
 
+
+def parse_args():
+    """
+    Parse command line arguments.
+    
+    Returns:
+        argparse.Namespace: Parsed command line arguments
+    """
+    parser = argparse.ArgumentParser(description='Trading Strategy Runner')
+    
+    # Model selection argument
+    parser.add_argument('--model', 
+                       type=str,
+                       choices=['qlearning', 'tree'],
+                       default='qlearning',
+                       help='Select which model to use (qlearning, tree)')
+    
+    # Common parameters
+    parser.add_argument('--symbol',
+                       type=str,
+                       default='JPM',
+                       help='Stock symbol to trade')
+    
+    parser.add_argument('--start-date',
+                       type=str,
+                       default='2008-01-01',
+                       help='Start date for training (YYYY-MM-DD)')
+    
+    parser.add_argument('--end-date',
+                       type=str,
+                       default='2009-01-01',
+                       help='End date for training (YYYY-MM-DD)')
+    
+    parser.add_argument('--test-start',
+                       type=str,
+                       default='2009-01-01',
+                       help='Start date for testing (YYYY-MM-DD)')
+    
+    parser.add_argument('--test-end',
+                       type=str,
+                       default='2010-01-01',
+                       help='End date for testing (YYYY-MM-DD)')
+    
+    parser.add_argument('--start-val',
+                       type=float,
+                       default=100000,
+                       help='Starting portfolio value')
+    
+    # Model-specific parameters
+    parser.add_argument('--impact',
+                       type=float,
+                       default=0.0,
+                       help='Market impact of each transaction')
+    
+    parser.add_argument('--commission',
+                       type=float,
+                       default=0.0,
+                       help='Commission amount charged per transaction')
+    
+    # QLearning specific parameters
+    parser.add_argument('--bins',
+                       type=int,
+                       default=10,
+                       help='Number of bins for QLearning discretization')
+    
+    parser.add_argument('--alpha',
+                       type=float,
+                       default=0.2,
+                       help='Learning rate for QLearning')
+    
+    parser.add_argument('--gamma',
+                       type=float,
+                       default=0.9,
+                       help='Discount factor for QLearning')
+    
+    return parser.parse_args()
+
+def create_model(args):
+    """
+    Create the appropriate model based on command line arguments.
+    
+    Args:
+        args (argparse.Namespace): Parsed command line arguments
+        
+    Returns:
+        object: Instance of the selected model
+    """
+    if args.model == 'qlearning':
+        return QLearningTrader(
+            impact=args.impact,
+            commission=args.commission,
+            bins=args.bins,
+            alpha=args.alpha,
+            gamma=args.gamma
+        )
+    elif args.model == 'tree':
+        return DecisionTreeTrader(
+            impact=args.impact,
+            commission=args.commission,
+            n_day_return=5,
+            y_buy=0.008,
+            y_sell=-0.008,
+            leaf_size=6,
+            num_bags=10)
+    return None
 
 def get_plot(trades_benchmark, trades_learner, symbol_="JPM", impact_=0.005, commission_=9.95,
              start_cash=100000, title="In Sample", fig_name="experiment1_in_sample.png"):
@@ -23,35 +129,44 @@ def get_plot(trades_benchmark, trades_learner, symbol_="JPM", impact_=0.005, com
     plt.savefig("./images/" + fig_name, bbox_inches="tight")
     plt.close()
 
-if __name__ == "__main__":
+def main():
+    args = parse_args()
+    
+    # Convert date strings to datetime objects
+    start_date = datetime.datetime.strptime(args.start_date, '%Y-%m-%d')
+    end_date = datetime.datetime.strptime(args.end_date, '%Y-%m-%d')
+    test_start = datetime.datetime.strptime(args.test_start, '%Y-%m-%d')
+    test_end = datetime.datetime.strptime(args.test_end, '%Y-%m-%d')
+    
+    # Create and train the model
+    model = create_model(args)
+    
+    # Train the model
+    model.train_model(
+        symbol=args.symbol,
+        sd=start_date,
+        ed=end_date,
+        sv=args.start_val
+    )
+    
+    # Test the model
+    trades_model_in = model.test_model(
+        symbol=args.symbol,
+        sd=start_date,
+        ed=end_date,
+        sv=args.start_val
+    )
 
+    trades_model_out = model.test_model(
+        symbol=args.symbol,
+        sd=test_start,
+        ed=test_end,
+        sv=args.start_val
+    )
 
-    # ---------------------------- #
-    #       DecisionTreeTrader     #
-    # ---------------------------- #
-    # The in-sample period is from January 1, 2008, to December 31, 2009.
-    # The out-of-sample/testing period is from January 1, 2010, to December 31, 2011.
-    # The starting cash is $100,000
-    start_date_in = datetime.datetime(2008, 1, 1)
-    end_date_in = datetime.datetime(2009, 12, 31)
-    start_date_out = datetime.datetime(2010, 1, 1)
-    end_date_out = datetime.datetime(2011, 12, 31)
-    cash = 100000
-    symbol_select = "JPM"
     impact_select = 0.005
     commission_select = 9.95
 
-    dt_model = dtt.DecisionTreeTrader(impact=impact_select,
-                                      commission=commission_select,
-                                      n_day_return=5,
-                                      y_buy=0.008,
-                                      y_sell=-0.008,
-                                      leaf_size=6,
-                                      num_bags=10)
-    dt_model.train_model(symbol=symbol_select, sd=start_date_in, ed=end_date_in)
-    trades_model_in = dt_model.test_model(symbol=symbol_select, sd=start_date_in, ed=end_date_in)
-    trades_model_out = dt_model.test_model(symbol=symbol_select, sd=start_date_out, ed=end_date_out)
-
     trades_benchmark_in = trades_model_in.copy()
     trades_benchmark_in.iloc[:, :] = 0.0
     trades_benchmark_in.iloc[0, 0] = 1000
@@ -60,43 +175,14 @@ if __name__ == "__main__":
     trades_benchmark_out.iloc[0, 0] = 1000
 
     # in-sample
-    get_plot(trades_benchmark_in, trades_model_in, symbol_=symbol_select, impact_=impact_select,
-             commission_=commission_select, start_cash=cash, title="In Sample", fig_name="experiment1_in_sample.png")
+    get_plot(trades_benchmark_in, trades_model_in, symbol_=args.symbol, impact_=impact_select,
+             commission_=commission_select, start_cash=args.start_val, title="In Sample", fig_name="experiment1_in_sample.png")
     # out-sample
-    get_plot(trades_benchmark_out, trades_model_out, symbol_=symbol_select, impact_=impact_select,
-             commission_=commission_select, start_cash=cash, title="Out of Sample",
+    get_plot(trades_benchmark_out, trades_model_out, symbol_=args.symbol, impact_=impact_select,
+             commission_=commission_select, start_cash=args.start_val, title="Out of Sample",
              fig_name="experiment1_out_sample.png")
 
 
-
-    # ---------------------------- #
-    #         QLearningTrader      #
-    # ---------------------------- #
-
-    ql_model = qlt.QLearningTrader(impact=impact_select,
-                                 commission=commission_select,
-                                 bins=10,
-                                 alpha=0.2,
-                                 gamma=0.9,
-                                 rar=0.98,
-                                 radr=0.999,
-                                 dyna=0)
-    ql_model.train_model(symbol=symbol_select, sd=start_date_in, ed=end_date_in)
-    trades_model_in = ql_model.test_model(symbol=symbol_select, sd=start_date_in, ed=end_date_in)
-    trades_model_out = ql_model.test_model(symbol=symbol_select, sd=start_date_out, ed=end_date_out)
-
-    trades_benchmark_in = trades_model_in.copy()
-    trades_benchmark_in.iloc[:, :] = 0.0
-    trades_benchmark_in.iloc[0, 0] = 1000
-    trades_benchmark_out = trades_model_out.copy()
-    trades_benchmark_out.iloc[:, :] = 0.0
-    trades_benchmark_out.iloc[0, 0] = 1000
-
-    # in-sample
-    get_plot(trades_benchmark_in, trades_model_in, symbol_=symbol_select, impact_=impact_select,
-             commission_=commission_select, start_cash=cash, title="In Sample", fig_name="experiment2_in_sample.png")
-    # out-sample
-    get_plot(trades_benchmark_out, trades_model_out, symbol_=symbol_select, impact_=impact_select,
-             commission_=commission_select, start_cash=cash, title="Out of Sample",
-             fig_name="experiment2_out_sample.png")
+if __name__ == "__main__":
+    main()
 
